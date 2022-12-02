d4c858c516c37243518ee6bfc24e1b2a51033d94
Prasang023 <prasang023@gmail.com>
2022-12-03 00:19:05 +0530

    redux added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,10 @@
+import { wrapper } from "../redux/store"
+
+// styles import
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
