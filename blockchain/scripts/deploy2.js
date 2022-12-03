44a183d69b804b354559e907fa4d0c581cd18f0f
Prakhar Ojha <prakharojha12@gmail.com>
2022-12-03 09:39:03 +0530

    Added contract for ZappNft

---
+++ blockchain/scripts/deploy2.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const saveFiles = require("./helper");
+async function main() {
+  
+
+  
+  const ZappNFT = await hre.ethers.getContractFactory("ZappTokenNFT");
+  const zappnft = await ZappNFT.deploy();
+
+  await zappnft.deployed();
+
+  await zappnft.transferOwnership("0x94e99f2247f855841e73a82c0562f2bfd760c1fe");
+  saveFiles(zappnft,"ZappTokenNFT");
+  console.log(
+    `Zapp NFT Contract deployed to ${zappnft.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
