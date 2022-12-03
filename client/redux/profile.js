8837e0e61e281669f9baf8d584e9a48a4d6db3a1
Prasang023 <prasang023@gmail.com>
2022-12-04 02:15:10 +0530

    profile lendings

--- client/redux/profile.js
+++ client/redux/profile.js
@@ -48,7 +48,8 @@ export const getMyLendings = createAsyncThunk(
   "profile/getMyLendings",
   async (_, thunkAPI) => {
     try {
-      const walletAddress = thunkAPI.getState().navbar.walletAddress
+      console.log("lendings dispatched")
+      const { walletAddress, instances } = thunkAPI.getState().navbar
       // const response = await axios.get(
       //   `${process.env.API_DOMAIN}/${walletAddress}/mylentnft`
       // )
@@ -59,24 +60,26 @@ export const getMyLendings = createAsyncThunk(
       const proposalHolder = []
       for (var i = 1; i <= bn.toNumber(); i++) {
         const currentProposalData = await instances.borrowRequests(i)
-        if (
-          currentProposalData[0] == walletAddress         )
+        console.log(currentProposalData)
+        if (currentProposalData[6] == walletAddress) {
           //add to check status too!
-          {
           const data = {
             borrower_address: currentProposalData[0],
             amount: currentProposalData[1].toNumber(),
             roi: currentProposalData[2].toNumber(),
             repay: currentProposalData[3].toNumber(),
             proposalid: currentProposalData[4].toNumber(),
-            nftURI:`https://testnets.opensea.io/assets/mumbai/${currentProposalData[8]}/${currentProposalData[9]}`,
-            whenBorrowed:currentProposalData[7].toNumber(),
+            nftURI: `https://testnets.opensea.io/assets/mumbai/${currentProposalData[8]}/${currentProposalData[9]}`,
+            whenBorrowed: currentProposalData[7].toNumber()
           }
           proposalHolder.push(data)
         }
       }
+      console.log(proposalHolder)
+      return proposalHolder
     } catch (err) {
       thunkAPI.dispatch(setError(err.response?.data?.message))
+      console.log(err)
       return thunkAPI.rejectWithValue(err.response?.data?.message)
     }
   }
